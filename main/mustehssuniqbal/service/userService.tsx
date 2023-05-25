3fb0866be81d6e8b33189019c7fe20c7e404be8b
Mustehssun <mustehssun@gmail.com>
2023-05-25 09:37:21 +0500

    userService created

--- main/mustehssuniqbal/service/userService.tsx
+++ main/mustehssuniqbal/service/userService.tsx
@@ -4,4 +4,11 @@ const rest = require("./rest");
 
 const resourceUrl: string = "/users";
 
-const getUser = (id: number): Promise<User> => rest.getRequest(`${resourceUrl}/id`);
+const getUser = (id: number): Promise<User> => rest.getRequest(`${resourceUrl}/${id}`);
+
+const updateUser = (id: number, user: User): Promise<User> => rest.postRequest(`${resourceUrl}/${id}`, user);
+
+export {
+    getUser,
+    updateUser
+};
